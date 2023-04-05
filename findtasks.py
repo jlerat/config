65547856a3cc4f1a0a230177494c90ca24ddb943
Julien Lerat <julien.lerat@csiro.au>
2023-04-05 13:43:16 +1000

    fix: removed numpy dependency in findtask

--- findtasks.py
+++ findtasks.py
@@ -1,18 +1,17 @@
 import sys
 from pathlib import Path
 import re
-import numpy as np
 
 folder = sys.argv[1]
 lf = list(Path(folder).glob('*.log'))
-tasks = np.array([int(re.search('(?<=TASK)[0-9]+', f.stem).group()) for f in lf])
+tasks = [int(re.search('(?<=TASK)[0-9]+', f.stem).group()) for f in lf]
 
-tasks = np.sort(tasks)
-expected = np.arange(0, tasks.max()+1)
+expected = [i for i in range(0, max(tasks)+1)]
+
 print(f"{len(expected)} log files expected, {len(tasks)} found.")
 
 if len(expected)>len(tasks):
-    missings = set(expected.tolist())-set(tasks.tolist())
+    missings = set(expected)-set(tasks)
     print("Missing log file for tasks")
     for miss in missings:
         print(f"{miss},")
