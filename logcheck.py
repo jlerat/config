05fed363e4d10481356854d159ad7c64ddc24514
Julien Lerat <julien.lerat@csiro.au>
2024-01-09 14:23:48 +1100

    fix: fixed task number ordering

--- logcheck.py
+++ logcheck.py
@@ -42,13 +42,13 @@ def grep(files, pattern, commands=''):
     return [f for f in g.decode().split("\n") if len(f)>0]
 
 def get_task(files):
-    return [int(re.search('(?<=TASK)[0-9]+', str(f)).group()) for f in files]
+    return [int(re.search('(?<=TASK)(-|)[0-9]+', str(f)).group()) for f in files]
 
 def printlist(objs, message, ind):
     if len(objs)>0:
         print(f"{indent(ind)}{message}:")
         for i in range(0, len(objs), ITEMS_PER_LINES):
-            o = [f"{int(n):4d}" for n in objs[i:i+ITEMS_PER_LINES]]
+            o = [f"{str(n):>4}" for n in objs[i:i+ITEMS_PER_LINES]]
             print(f"{indent(ind+1)}"+" ".join(o))
 
 
@@ -58,9 +58,11 @@ def printlist(objs, message, ind):
 print(f"\n----------- ERROR FILES --------------")
 lf = list(FOLDER.glob('*.out'))
 if len(lf)>0:
-    jobs = list(set([re.sub('.*_JOB', '', f.stem) for f in lf]))
+    jobs = list(set([re.sub('.*JOB', '', f.stem) for f in lf]))
     for jobid in jobs:
-        print(f"{indent(1)}JOB ID {jobid}")
+        ndigits = 3
+        txt = " ".join([jobid[i:i+ndigits] for i in range(0, len(jobid), ndigits)])
+        print(f"{indent(1)}JOB ID [{txt}]")
 
         # Count error files
         l = ls(f"*JOB{jobid}.err")
@@ -72,7 +74,9 @@ if len(lf)>0:
 
         # Print error file numbers
         if len(g)>0:
-            errn = [re.sub(".*_TASK|_JOB.*", "", f) for f in g]
+            errn = [int(re.sub(".*_TASK|_JOB.*", "", f)) for f in g]
+            errn.sort()
+            errn = [f"{t:3d}" for t in errn]
             printlist(errn, "Task IDs of files with error", 2)
         print("\n")
 else:
@@ -103,15 +107,19 @@ if len(flogs)>0:
     print(f"{indent(1)}Files with errors    : {len(ge)}")
 
     d = list(set(expected)-set(tasks))
+    d.sort()
     printlist(d, "IDs of missing logs files", 1)
 
     d = list(set(started)-set(completed))
+    d.sort()
     printlist(d, "IDs of processes started but not completed", 1)
 
     d = list(set(expected)-set(started))
+    d.sort()
     printlist(d, "IDs of processes not started", 1)
 
     e = get_task(ge)
+    e.sort()
     printlist(e, "IDs of process with errors", 1)
 else:
     print("No log files found")
